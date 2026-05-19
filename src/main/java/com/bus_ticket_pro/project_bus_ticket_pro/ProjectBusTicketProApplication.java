58fcb306a0b78ec7e6383aa980410c8497f2a253
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-05-19 08:45:48 +0700

    init-project

---
+++ src/main/java/com/bus_ticket_pro/project_bus_ticket_pro/ProjectBusTicketProApplication.java
@@ -0,0 +1,13 @@
+package com.bus_ticket_pro.project_bus_ticket_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBusTicketProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBusTicketProApplication.class, args);
+    }
+
+}
