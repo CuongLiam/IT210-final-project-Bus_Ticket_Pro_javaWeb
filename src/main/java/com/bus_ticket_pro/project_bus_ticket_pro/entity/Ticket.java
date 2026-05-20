001d53aba8111a6c80de7559dbcd4182f9ed4806
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-05-20 09:22:14 +0700

    ticket

---
+++ src/main/java/com/bus_ticket_pro/project_bus_ticket_pro/entity/Ticket.java
@@ -0,0 +1,40 @@
+package com.bus_ticket_pro.project_bus_ticket_pro.entity;
+
+import com.bus_ticket_pro.project_bus_ticket_pro.entity.base.BaseEntity;
+import com.bus_ticket_pro.project_bus_ticket_pro.enums.TicketStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+public class Ticket extends BaseEntity {
+
+    @Column(name = "ticket_code", nullable = false, unique = true, length = 30)
+    private String ticketCode;
+
+    @Column(name = "customer_name", nullable = false, length = 100)
+    private String customerName;
+
+    @Column(nullable = false, length = 30)
+    private String phone;
+
+    @Column(length = 100)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "seat_id", nullable = false)
+    private Seat seat;
+
+    @Column(name = "total_price", nullable = false, precision = 12, scale = 2)
+    private BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TicketStatus status = TicketStatus.PENDING;
+
+}
